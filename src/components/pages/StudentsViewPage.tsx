d1e813fb4209cd4a71f042d5820fd2aaea7bd7b8
SYED175 <syedismailgp@gmail.com>
2024-10-08 08:50:01 +0530

    Add messsage component in StudentsViewPage

--- src/components/pages/StudentsViewPage.tsx
+++ src/components/pages/StudentsViewPage.tsx
@@ -11,6 +11,7 @@ import AlertDestructive from "../common/AlertDestructive";
 import { useDispatch } from "react-redux";
 import { authActions } from "@/store/AuthSlice";
 import { useMediaQuery } from "react-responsive";
+import Message from "../common/Message";
 
 const StudentsViewPage: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -82,66 +83,74 @@ const StudentsViewPage: React.FC = () => {
   return (
     <div className="w-full">
       <NavBar />
-      <div className="md:px-6 py-8">
-        <div className="mb-4 flex items-center">
-          <div className="relative flex-grow">
-            <input
-              type="text"
-              placeholder="Search for a student by name or email"
-              className="w-full text-xs md:text-base pl-10 pr-4 py-2 border rounded-md"
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <Search
-              className="absolute left-3 top-2.5 text-gray-400"
-              size={isMd ? 20 : 15}
-            />
-          </div>
+      {students.length === 0 ? (
+        <div className="text-center">
+          <Message message="No students present..." />
         </div>
-        <table className="w-full">
-          <thead>
-            <tr className="text-left text-xs md:text-base border-b">
-              <th className="py-2 px-2">Student Photo</th>
-              <th className=" py-2 px-2">Name</th>
-              <th className="  py-2 px-2">Email</th>
-              <th className="py-2 px-2">Age</th>
-              <th className="py-2 px-2">Gender</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredStudents?.map((student, index) => (
-              <tr
-                key={index}
-                className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
-              >
-                <td className="py-3 px-2  flex text-xs md:text-base items-center">
-                  <div className="w-6 h-6 sm:w-8 sm:h-8 md:w-11 md:h-11 rounded-full bg-gray-300 mr-3">
-                    <img src={John} className="rounded-full" alt="" />
-                  </div>
-                </td>
-                <td className="   py-3 px-2 text-xs md:text-base">
-                  {student.studentName}
-                </td>
-                <td className="  py-3 px-2 text-xs md:text-base">
-                  {student.email}
-                </td>
-
-                <td className="py-3 px-2 text-xs md:text-base">
-                  {student.age}
-                </td>
-                <td className="py-3 px-2 text-xs md:text-base">
-                  {student.gender}
-                </td>
+      ) : (
+        <div className="md:px-6 py-8">
+          <div className="mb-4 flex items-center">
+            <div className="relative flex-grow">
+              <input
+                type="text"
+                placeholder="Search for a student by name or email"
+                className="w-full text-xs md:text-base pl-10 pr-4 py-2 border rounded-md"
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <Search
+                className="absolute left-3 top-2.5 text-gray-400"
+                size={isMd ? 20 : 15}
+              />
+            </div>
+          </div>
+          <table className="w-full">
+            <thead>
+              <tr className="text-left text-xs md:text-base border-b">
+                <th className="py-2 px-2">Student Photo</th>
+                <th className=" py-2 px-2">Name</th>
+                <th className="  py-2 px-2">Email</th>
+                <th className="py-2 px-2">Age</th>
+                <th className="py-2 px-2">Gender</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {filteredStudents?.map((student, index) => (
+                <tr
+                  key={index}
+                  className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                >
+                  <td className="py-3 px-2  flex text-xs md:text-base items-center">
+                    <div className="w-6 h-6 sm:w-8 sm:h-8 md:w-11 md:h-11 rounded-full bg-gray-300 mr-3">
+                      <img src={John} className="rounded-full" alt="" />
+                    </div>
+                  </td>
+                  <td className="   py-3 px-2 text-xs md:text-base">
+                    {student.studentName}
+                  </td>
+                  <td className="  py-3 px-2 text-xs md:text-base">
+                    {student.email}
+                  </td>
 
-      <PaginationShad
-        currentPage={currentPage}
-        handlePageChange={onPageChange}
-        totalPages={totalPages}
-      />
+                  <td className="py-3 px-2 text-xs md:text-base">
+                    {student.age}
+                  </td>
+                  <td className="py-3 px-2 text-xs md:text-base">
+                    {student.gender}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+
+      {!(students.length === 0) && (
+        <PaginationShad
+          currentPage={currentPage}
+          handlePageChange={onPageChange}
+          totalPages={totalPages}
+        />
+      )}
     </div>
   );
 };
