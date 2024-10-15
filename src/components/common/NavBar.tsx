ba175244484192c7539afd9d379708970741c29c
SYED175 <syedismailgp@gmail.com>
2024-10-15 18:37:44 +0530

    Change logout button style with lucid-react

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,9 +1,10 @@
 import { IoIosNotificationsOutline } from "react-icons/io";
-import { PiUserCircleCheckLight } from "react-icons/pi";
 import { useDispatch } from "react-redux";
 import { authActions } from "../../store/AuthSlice";
 import Profile from "./Profile";
 import apiClient from "@/services/api-client";
+import { LogOut } from "lucide-react";
+import { Button } from "../ui/button";
 
 const NavBar = () => {
   const dispatch = useDispatch();
@@ -18,20 +19,20 @@ const NavBar = () => {
       .catch((error) => console.log(error.response.data));
   };
   return (
-    <div className="hidden md:flex w-full  h-24 bg-white justify-between items-center px-7 mb-7">
+    <div className="hidden md:flex w-full  h-24  bg-white justify-between items-center px-7 mb-7">
       <div className="flex gap-3 items-center">
-        <PiUserCircleCheckLight className="size-9 text-green-500" />
         <Profile />
       </div>
 
       <div className="flex items-center gap-5">
         <IoIosNotificationsOutline className="size-7" />
-        <button
+        <Button
+          variant="secondary"
           onClick={handleLogout}
-          className=" w-28 h-10 rounded text-white bg-custom-blue active:bg-blue-300 hover:bg-blue-400 text-sm font-semibold"
+          className=" text-purple-600  hover:text-red-400 hover:bg-black bg-white  transition-colors duration-200"
         >
-          Log out
-        </button>
+          <LogOut className="mr-2 h-4 w-4" /> Logout
+        </Button>
       </div>
     </div>
   );
